9b93c713a2c46ce219a0dc6177e5bbbaba49e258
qus0in <qus0in@gmail.com>
2025-03-31 13:24:43 +0900

    아... 아...

--- src/main/java/org/example/mealisallyouneed/config/MyBatisConfig.java
+++ src/main/java/org/example/mealisallyouneed/config/MyBatisConfig.java
@@ -7,10 +7,12 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
+import java.util.logging.Logger;
 
 
 public class MyBatisConfig {
     private static final SqlSessionFactory sqlSessionFactory;
+    private static final Logger logger = Logger.getLogger(MyBatisConfig.class.getName());
 
     static {
         // dotenv 로드
@@ -22,16 +24,21 @@ public class MyBatisConfig {
         properties.setProperty("DB_URL", dotenv.get("DB_URL"));
         properties.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
         properties.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+        logger.info("MyBatisConfig: " + properties);
 
         // mybatis-config.xml 파일 로드
         String resource = "mybatis-config.xml";
-        // 등록 안해도 코드에서 찾아요 (resource 에 등록 안해도 수동으로 찾는 코드)
+        // 아니야... 등록을 해야 찾아줘... 속지마...
         try (InputStream inputStream = MyBatisConfig.class.getClassLoader().getResourceAsStream(resource)) {
+            logger.info("%s".formatted(inputStream));
+            logger.info(String.valueOf(MyBatisConfig.class.getClassLoader().getResource(resource)));
+
             // 연결...
             sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream, properties);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+        logger.info("config 완료");
     }
 
     // 일종의 싱글톤으로 봐야하는데...
